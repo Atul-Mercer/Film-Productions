e08929c58c81fdb3c6b089015d5be8aaa73b8ef0
Atul <atul@Atuls-MacBook-Air.local>
2025-11-11 07:55:25 +0530

    Done with this

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -28,7 +28,7 @@ export function Navigation({ currentPage, setCurrentPage }: NavigationProps) {
   };
 
   return (
-    <nav className="navbar" >
+    <nav className="navbar" style={{ height: '80px' }}>
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8" >
         <div className="flex justify-between items-center h-16" >
           {/* Logo */}
